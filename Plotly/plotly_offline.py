d3ec226980658301c79915f1d52a2e70e81ef43f
AlexMiao822 <miaoxl8822@hotmail.com>
2017-04-04 12:34:03 -0400

    Delete plotly_offline.py

--- Plotly/plotly_offline.py
+++
@@ -1,9 +0,0 @@
-## Plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-init_notebook_mode(connected=True)  # use plotly.offline (injects the plotly.js source files into the notebook)
-from plotly import tools
-import plotly.tools as tls
-
-
