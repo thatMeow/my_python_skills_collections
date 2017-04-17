559918e1c91646adcc777ff3c1f238aacdf3f59f
thatMeow <thatMeow@users.noreply.github.com>
2017-04-17 18:19:05 -0400

    Create Divide columns by a list of numbers.py

---
+++ Data_Manipulation/Divide columns by a list of numbers.py
@@ -0,0 +1,3 @@
+# Divide columns by a list of numbers. If the number is zero, make the column "1".
+# Details can be found here:
+# http://stackoverflow.com/questions/43458454/python-divide-data-frame-by-a-list-of-numbers-zero-included/43458646?noredirect=1#comment73973434_43458646
