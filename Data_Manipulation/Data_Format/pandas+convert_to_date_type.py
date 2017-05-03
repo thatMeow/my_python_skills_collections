3a9c361e93c9cdbf7fac0706c7b71dd4ebd002cc
thatMeow <thatMeow@users.noreply.github.com>
2017-05-03 09:25:18 -0400

    Update and rename convert_to_date_type_in_pandas.py to pandas+convert_to_date_type.py

--- Data_Manipulation/Data_Format/pandas+convert_to_date_type.py
+++ Data_Manipulation/Data_Format/pandas+convert_to_date_type.py
@@ -5,3 +5,6 @@ from pandas.tseries.offsets import MonthEnd
 from datetime import datetime
 import datetime
 
+df['col'] = pd.to_datetime(df['col']) + MonthEnd(1) # change vintage to month end
+df['col'] = df['col'].dt.date
+
