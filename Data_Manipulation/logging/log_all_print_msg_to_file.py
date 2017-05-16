12522837f1e98776e4f7a3c1c59d4b6cd41367c1
thatMeow <thatMeow@users.noreply.github.com>
2017-05-16 17:38:13 -0400

    Update log_all_print_msg_to_file.py

--- Data_Manipulation/logging/log_all_print_msg_to_file.py
+++ Data_Manipulation/logging/log_all_print_msg_to_file.py
@@ -1,5 +1,6 @@
-# http://stackoverflow.com/questions/44006814/python-automatically-writing-all-print-statements-to-a-log-fil/44007027#44007027
+# http://stackoverflow.com/questions/44006814/python-automatically-writing-all-print-statements-to-a-log-fil/44007027?noredirect=1#comment75046253_44007027
 
+# This code only saves print messages to a log file, the command line will not have the messages printed out
 import sys
 sys.stdout = open('log.txt', 'r')
 
@@ -8,3 +9,18 @@ import pandas
 print "pandas library imported"
 df = pd.Dataframe(...)
 print "df created."
+
+# the following code will write messages to command line and save print messages to a file
+
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+
+f = open('logfile', 'w')
+backup = sys.stdout
+sys.stdout = Tee(sys.stdout, f)
+
+print "hello world"  # this should appear in stdout and in file
