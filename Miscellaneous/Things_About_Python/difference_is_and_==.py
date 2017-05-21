76421418096e3134020c9f9f761e1cdb7d8f9df7
thatMeow <thatMeow@users.noreply.github.com>
2017-05-21 11:09:56 -0400

    Create difference_is_and_==.py

---
+++ Miscellaneous/Things_About_Python/difference_is_and_==.py
@@ -0,0 +1,13 @@
+# http://stackoverflow.com/questions/132988/is-there-a-difference-between-and-is-in-python
+
+# "is" returns True if two variables point to the same object.
+# "==" returns True if objected referred by the variables are equal
+
+# e.g.
+
+1000 is 10**3
+False
+
+1000 == 10**3
+True
+
