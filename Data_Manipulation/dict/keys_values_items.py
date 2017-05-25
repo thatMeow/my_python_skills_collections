d28be4fe9410896ed980364537d7794bd447aca4
thatMeow <thatMeow@users.noreply.github.com>
2017-05-25 10:17:23 -0400

    Create keys_values_items.py

---
+++ Data_Manipulation/dict/keys_values_items.py
@@ -0,0 +1,12 @@
+# for dict, use keys, values, items to access dicts
+
+>>> list(pos.keys())
+['colorless', 'furiously', 'sleep', 'ideas']
+
+>>> list(pos.values())
+['ADJ', 'ADV', 'V', 'N']
+
+>>> list(pos.items())
+[('colorless', 'ADJ'), ('furiously', 'ADV'), ('sleep', 'V'), ('ideas', 'N')]
+
+
