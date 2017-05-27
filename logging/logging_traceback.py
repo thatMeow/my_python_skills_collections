1a122ab40587cc69d108fc8194757715f594705f
thatMeow <thatMeow@users.noreply.github.com>
2017-05-27 15:57:26 -0400

    Update logging_traceback.py

--- logging/logging_traceback.py
+++ logging/logging_traceback.py
@@ -1,1 +1,14 @@
+# https://stackoverflow.com/questions/1508467/log-exception-with-traceback
+# Use logging.exception() to raise "traceback"
 
+import logging
+LOG_FILENAME = 'ogging_example.out'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+logging.debug('This message should go to the log file')
+
+try:
+    run_my_stuff()
+except:
+    logging.exception('Got exception on main handler')
+    raise
