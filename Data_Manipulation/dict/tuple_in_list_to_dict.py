d18054da3b1c13840405b0bac7cf0a2fbb7471c8
thatMeow <thatMeow@users.noreply.github.com>
2017-05-31 09:43:41 -0400

    Update tuple_in_list_to_dict.py

--- Data_Manipulation/dict/tuple_in_list_to_dict.py
+++ Data_Manipulation/dict/tuple_in_list_to_dict.py
@@ -1,5 +1,4 @@
 # https://stackoverflow.com/questions/31490101/create-a-dict-with-unique-keys-and-various-list-values-from-a-tuple
-
 # Convert a list of tuples to a dict that has keys and values. If the keys have multiple values turn the values into a list
 
 >>> aaa = list((('a', 1), ('b', 2), ('c', 3), ('a', 'zzz')))
