71c1eda272f1689da33ac9343ee75e8b78a633a6
thatMeow <thatMeow@users.noreply.github.com>
2017-06-27 22:26:46 -0400

    Create dict_with_order.py

---
+++ Data_Manipulation/dict/dict_with_order.py
@@ -0,0 +1,17 @@
+
+
+from collections import OrderedDict
+
+od = OrderedDict(['a':1, 'b':2, 'c':3, 'd':4])
+
+This is an Ordered Dict:
+('a', 1)
+('b', 2)
+('c', 3)
+('d', 4)
+
+This is a Dict:
+('a', 1)
+('c', 3)
+('b', 2)
+('d', 4)
