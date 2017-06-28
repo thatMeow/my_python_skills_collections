d7dcb7dfe823f6cac3157a1036e9fd88500e29ce
thatMeow <thatMeow@users.noreply.github.com>
2017-06-28 11:39:31 -0400

    Create iterate_vars_and_format.py

---
+++ Data_Manipulation/Data_Format/iterate_vars_and_format.py
@@ -0,0 +1,4 @@
+
+# Iterate through the columns in a dataframe and format each column
+# https://stackoverflow.com/questions/32744997/python-pandas-apply-formatting-to-each-column-in-dataframe-using-a-dict-mapping
+
