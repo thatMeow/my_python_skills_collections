d12812bb7ca3519b416ab8204b419c1e27cb70bc
thatMeow <thatMeow@users.noreply.github.com>
2017-07-19 10:20:08 -0400

    Create setup.py

---
+++ all_other_topic/emails/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ponymail",
+    version="1.0",
+    url="https://github.kdc.capitalone.com/fiq831/ponymail",
+    packages=["ponymail"],
+    zip_safe=False
+)
