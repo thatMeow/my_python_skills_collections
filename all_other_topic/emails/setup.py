75748de5d2577dffc17ca28e3db7a79f303e32de
thatMeow <thatMeow@users.noreply.github.com>
2017-07-19 21:21:16 -0400

    Delete setup.py

--- all_other_topic/emails/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="ponymail",
-    version="1.0",
-    url="https://github.kdc.capitalone.com/fiq831/ponymail",
-    packages=["ponymail"],
-    zip_safe=False
-)
