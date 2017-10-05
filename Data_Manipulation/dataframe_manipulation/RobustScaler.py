afd577dae7471ff830ce22f228db23b408a58ca5
thatMeow <thatMeow@users.noreply.github.com>
2017-10-05 10:37:16 -0400

    Update RobustScaler.py

--- Data_Manipulation/dataframe_manipulation/RobustScaler.py
+++ Data_Manipulation/dataframe_manipulation/RobustScaler.py
@@ -1,1 +1,10 @@
+# http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.RobustScaler.html
 
+
+from sklearn.preprocessing import RobustScaler
+
+scaler = RobustScaler()
+X = pd.DataFrame(df.drop(['target'], axis=1))
+X = scaler.fit_transform(X)
+X = pd.DataFrame(X, columns=X.columns)
+X.head()
