2f428498ef847640524612114387476e4abc816f
thatMeow <thatMeow@users.noreply.github.com>
2017-10-05 10:30:31 -0400

    Update convert_to_dummies.py

--- Data_Manipulation/dataframe_manipulation/convert_to_dummies.py
+++ Data_Manipulation/dataframe_manipulation/convert_to_dummies.py
@@ -1,8 +1,7 @@
 # convert a column in a DataFrame to dummies variables and delete the column
 
 def convert_to_dummies(df, column):
-    dummies = pd.get_dummies(df[column])
+
+    dummies = pd.get_dummies(df[column]).rename(columns=lambda x: column + '_' + str(x))
     df = pd.concat([df, dummies], axis=1)
-    df = df.drop(column, axis=1) #when dropping column don't forget "axis=1"
-    
-    return df
+    df = df.drop(column, axis=1) 
