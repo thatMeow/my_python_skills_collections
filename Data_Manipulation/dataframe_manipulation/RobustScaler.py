e220ff03ee1729991e1836b85db46298d9c07f90
thatMeow <thatMeow@users.noreply.github.com>
2017-10-05 10:48:57 -0400

    Update RobustScaler.py

--- Data_Manipulation/dataframe_manipulation/RobustScaler.py
+++ Data_Manipulation/dataframe_manipulation/RobustScaler.py
@@ -4,7 +4,10 @@
 from sklearn.preprocessing import RobustScaler
 
 scaler = RobustScaler()
-X = pd.DataFrame(df.drop(['target'], axis=1))
-X = scaler.fit_transform(X)
-X = pd.DataFrame(X, columns=X.columns)
-X.head()
+X = pd.DataFrame(df.drop(['Churn?'], axis=1))
+robust_scaled_df = scaler.fit_transform(X)
+robust_scaled_df = pd.DataFrame(robust_scaled_df, columns=X.columns)
+
+# concat
+df = pd.concat([robust_scaled_df, df['Churn?']], axis=1)
+df.head()
