eb43018624105774e288041625ac11f16af3147c
thatMeow <thatMeow@users.noreply.github.com>
2017-11-02 09:14:51 -0400

    Update apscheduler.py

--- Automations/scheduling/apscheduler.py
+++ Automations/scheduling/apscheduler.py
@@ -6,3 +6,7 @@ def my_job():
     
 scheduler.add_job(my_job, trigger='cron', hour='*', minute='*', second='*')
 scheduler.start()
+
+
+# If the function has parameters, use scheduler.add_job(lambda my_job(parameter_1, parameter_2), \
+#                                       trigger='cron', hour='*', minute='*', second='*')
