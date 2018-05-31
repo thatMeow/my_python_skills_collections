d47f891a1a081e45293338a79e3952db6ba9d7c5
thatMeow <thatMeow@users.noreply.github.com>
2018-05-31 19:33:10 -0400

    Create plot_heatmap_triangle.py

---
+++ Plotting/Seaborn/plot_heatmap_triangle.py
@@ -0,0 +1,21 @@
+sns.set(style="white")
+
+# Generate a large random dataset
+temp3 = train.copy()
+
+# Compute the correlation matrix
+corr = temp3.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(15, 13))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
