904f5df91e64d6a6277517d3aa15574ed1e6c007
thatMeow <thatMeow@users.noreply.github.com>
2018-07-07 10:35:18 -0400

    Update convert_to_dummies.py

--- Data_Manipulation/dataframe_manipulation/convert_to_dummies.py
+++ Data_Manipulation/dataframe_manipulation/convert_to_dummies.py
@@ -2,6 +2,7 @@
 
 def convert_to_dummies(df, column):
 
-    dummies = pd.get_dummies(df[column]).rename(columns=lambda x: column + '_' + str(x))
+    dummies = pd.get_dummies(df[column], prefix=[column])
     df = pd.concat([df, dummies], axis=1)
     df = df.drop(column, axis=1) 
+    return df
