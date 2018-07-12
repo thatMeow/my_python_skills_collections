eebd6459947f99b772206835e03f6597043526fe
thatMeow <thatMeow@users.noreply.github.com>
2018-07-12 13:40:33 -0400

    Update qcut.py

--- Data_Manipulation/dataframe_manipulation/qcut.py
+++ Data_Manipulation/dataframe_manipulation/qcut.py
@@ -1,5 +1,5 @@
 df['decile'] = pd.qcut(df['score'], 10, labels=np.arange(10, 0, -1))
 
 # get score cutoffs
-df = f1.groupby(['decile']).agg({'freedom_gate_predict_score' : [np.min, np.max]})
-df.columns = ['_'.join(col).strip() for col in df.columns.values]
+cutoffs = df.groupby(['decile']).agg({'score' : [np.min, np.max]})
+cutoffs.columns = ['_'.join(col).strip() for col in df.columns.values]
