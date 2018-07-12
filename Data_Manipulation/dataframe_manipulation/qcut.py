51f3ace79a128799ff57451a9b9ac80a89516fdc
thatMeow <thatMeow@users.noreply.github.com>
2018-07-12 13:42:32 -0400

    Update qcut.py

--- Data_Manipulation/dataframe_manipulation/qcut.py
+++ Data_Manipulation/dataframe_manipulation/qcut.py
@@ -2,4 +2,4 @@ df['decile'] = pd.qcut(df['score'], 10, labels=np.arange(10, 0, -1))
 
 # get score cutoffs
 cutoffs = df.groupby(['decile']).agg({'score' : [np.min, np.max]})
-cutoffs.columns = ['_'.join(col).strip() for col in df.columns.values]
+cutoffs.columns = ['_'.join(col).strip() for col in cutoffs.columns.values]
