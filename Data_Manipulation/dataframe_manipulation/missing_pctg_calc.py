30a4a358157d3f9d6e64fa132de9e2df7aa70fa1
thatMeow <thatMeow@users.noreply.github.com>
2019-06-14 16:35:54 -0400

    Update missing_pctg_calc.py

--- Data_Manipulation/dataframe_manipulation/missing_pctg_calc.py
+++ Data_Manipulation/dataframe_manipulation/missing_pctg_calc.py
@@ -1,3 +1,19 @@
+def null_values(df):
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        print ("Dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        return mis_val_table_ren_columns
+
+-------------------------------------------------------------------------------------------------------------------------------
+
 ecpi_na = (ecpi.isnull().sum() / len(ecpi)) * 100
 ecpi_na = ecpi_na.drop(ecpi_na[ecpi_na == 0].index).sort_values(ascending=False)[:30]
 missing_data = pd.DataFrame({'Missing Ratio' :ecpi_na})
